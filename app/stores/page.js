879c8548271ecd9d13241cb1b4b99846e1aded79
fatiya17 <fatiyalabibah17@gmail.com>
2026-07-22 07:14:18 +0700

    style: change store grid layout to max 4 columns on desktop

--- app/stores/page.js
+++ app/stores/page.js
@@ -29,7 +29,7 @@ export default async function StoresPage() {
         </div>
 
         {/* Store Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 sm:gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-4">
           {stores.map((store) => (
             <StoreCard key={store.id} store={store} />
           ))}
